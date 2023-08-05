06a734229cdbc66870956d6fbb022b45d9ae779a
LE LOUER Lucas <lucaslelouer@gmail.com>
2023-08-05 10:50:55 +0200

    Added software support
    Tkinter is messing with loops, will try to update to PyQt5

--- macro.py
+++ macro.py
@@ -1,33 +1,70 @@
-import pyautogui
-from keyboard import is_pressed
 from pynput import mouse, keyboard
 from pynput.mouse import Button
+from pynput.keyboard import Key
+from tkinter import *
 import time
 import threading
 import json
 import os
 
-
-macroName = input("Quelle nom tu veux donner a ton record: ")
-
-macroEvents = {'events':[]}
 mouseControl = mouse.Controller()
 keyboardControl = keyboard.Controller()
+special_keys = {"Key.shift": Key.shift, "Key.tab": Key.tab, "Key.caps_lock": Key.caps_lock, "Key.ctrl": Key.ctrl, "Key.ctrl_l": Key.ctrl_l, "Key.alt": Key.alt, "Key.cmd": Key.cmd, "Key.cmd_r": Key.cmd_r, "Key.alt_r": Key.alt_r, "Key.ctrl_r": Key.ctrl_r, "Key.shift_r": Key.shift_r, "Key.enter": Key.enter, "Key.backspace": Key.backspace, "Key.f19": Key.f19, "Key.f18": Key.f18, "Key.f17": Key.f17, "Key.f16": Key.f16, "Key.f15": Key.f15, "Key.f14": Key.f14, "Key.f13": Key.f13, "Key.media_volume_up": Key.media_volume_up, "Key.media_volume_down": Key.media_volume_down, "Key.media_volume_mute": Key.media_volume_mute, "Key.media_play_pause": Key.media_play_pause, "Key.f6": Key.f6, "Key.f5": Key.f5, "Key.right": Key.right, "Key.down": Key.down, "Key.left": Key.left, "Key.up": Key.up, "Key.page_up": Key.page_up, "Key.page_down": Key.page_down, "Key.home": Key.home, "Key.end": Key.end, "Key.delete": Key.delete, "Key.space": Key.space}
+
+
+
+def startRecord():
+    global start_time
+    global mouse_listener
+    global keyboard_listener
+    global macroEvents
+    macroEvents = {'events': []}
+    start_time = time.time()
+    mouse_listener = mouse.Listener(on_move=on_move, on_click=on_click, on_scroll=on_scroll)
+    keyboard_listener = keyboard.Listener(on_press=on_press, on_release=on_release)
+    print('record started')
+    with mouse_listener, keyboard_listener:
+        mouse_listener.join()
+        keyboard_listener.join()
+
+def stopRecord():
+    mouse_listener.stop()
+    keyboard_listener.stop()
+    json_macroEvents = json.dumps(macroEvents, indent=4)
+    with open(os.path.join("C:/Users/Lucas/Desktop/autre/projey python/macro-recorder/data/test.json"), "w") as macroRecord:
+        macroRecord.write(json_macroEvents)
+    print('record stopped')
+
+
+
+
+
+# def saveMacro():
+#     json_macroEvents = json.dumps(macroEvents, indent=4)
+#     with open(os.path.join("C:/Users/lucas/OneDrive/Bureau/projet/data", macroName + ".json"), "w") as macroRecord:
+#         macroRecord.write(json_macroEvents)
+
+
 
 def on_move(x, y):
     global start_time
     macroEvents["events"].append({'type': 'cursorMove', 'x': x, 'y': y, 'timestamp': time.time() - start_time})
     start_time = time.time()
 
+
 def on_click(x, y, button, pressed):
     global start_time
     if button == Button.left:
-        macroEvents["events"].append({'type': 'leftClickEvent', 'x': x, 'y': y, 'timestamp': time.time() - start_time, 'pressed': pressed})
+        macroEvents["events"].append(
+            {'type': 'leftClickEvent', 'x': x, 'y': y, 'timestamp': time.time() - start_time, 'pressed': pressed})
     elif button == Button.right:
-        macroEvents["events"].append({'type': 'rightClickEvent', 'x': x, 'y': y, 'timestamp': time.time() - start_time, 'pressed': pressed})
+        macroEvents["events"].append(
+            {'type': 'rightClickEvent', 'x': x, 'y': y, 'timestamp': time.time() - start_time, 'pressed': pressed})
     elif button == Button.middle:
-        macroEvents["events"].append({'type': 'middleClickEvent', 'x': x, 'y': y, 'timestamp': time.time() - start_time, 'pressed': pressed})
+        macroEvents["events"].append(
+            {'type': 'middleClickEvent', 'x': x, 'y': y, 'timestamp': time.time() - start_time, 'pressed': pressed})
     start_time = time.time()
+
 
 def on_scroll(x, y, dx, dy):
     global start_time
@@ -38,43 +75,53 @@ def on_scroll(x, y, dx, dy):
 def on_press(key):
     global start_time
     try:
-        macroEvents["events"].append({'type': 'keyboardEvent', 'key': key.char, 'timestamp': time.time() - start_time, 'pressed': True})
+        macroEvents["events"].append(
+            {'type': 'keyboardEvent', 'key': key.char, 'timestamp': time.time() - start_time, 'pressed': True})
     except AttributeError:
-        macroEvents["events"].append({'type': 'keyboardEvent', 'key': str(key), 'timestamp': time.time() - start_time, 'pressed': True})
+        macroEvents["events"].append(
+            {'type': 'keyboardEvent', 'key': str(key), 'timestamp': time.time() - start_time, 'pressed': True})
+    if key.char == 'o':
+        stopRecord()
     start_time = time.time()
+
 
 def on_release(key):
     global start_time
     try:
-        macroEvents["events"].append({'type': 'keyboardEvent', 'key': key.char, 'timestamp': time.time() - start_time, 'pressed': False})
+        macroEvents["events"].append(
+            {'type': 'keyboardEvent', 'key': key.char, 'timestamp': time.time() - start_time, 'pressed': False})
     except AttributeError:
-        macroEvents["events"].append({'type': 'keyboardEvent', 'key': str(key), 'timestamp': time.time() - start_time, 'pressed': False})
+        macroEvents["events"].append(
+            {'type': 'keyboardEvent', 'key': str(key), 'timestamp': time.time() - start_time, 'pressed': False})
     start_time = time.time()
 
-def detectStop():
-    global record
-    while record:
-        if is_pressed('s'):
-            mouse_listener.stop()
-            keyboard_listener.stop()
-            record = False
 
-
-time.sleep(1)
-print('enrengistrement lancÃ©')
-record = True
-start_time = time.time()
-mouse_listener = mouse.Listener(on_move=on_move, on_click=on_click, on_scroll=on_scroll)
-keyboard_listener = keyboard.Listener(on_press=on_press, on_release=on_release)
-
-with mouse_listener, keyboard_listener:
-    detectStop()
-    mouse_listener.join()
-    keyboard_listener.join()
-
-
-print('record fini')
-json_macroEvents = json.dumps(macroEvents, indent=4)
-
-with open(os.path.join("C:/Users/lucas/OneDrive/Bureau/projet/data", macroName+".json"), "w") as macroRecord:
-    macroRecord.write(json_macroEvents)
+def playRec():
+    print('record playing')
+    for i in range(len(macroEvents["events"])):
+        time.sleep(macroEvents["events"][i]["timestamp"])
+        if macroEvents["events"][i]["type"] == "cursorMove":
+            mouseControl.position = (macroEvents["events"][i]["x"], macroEvents["events"][i]["y"])
+        elif macroEvents["events"][i]["type"] == "leftClickEvent":
+            if macroEvents["events"][i]["pressed"] == True:
+                mouseControl.press(Button.left)
+            else:
+                mouseControl.release(Button.left)
+        elif macroEvents["events"][i]["type"] == "rightClickEvent":
+            if macroEvents["events"][i]["pressed"] == True:
+                mouseControl.press(Button.right)
+            else:
+                mouseControl.release(Button.right)
+        elif macroEvents["events"][i]["type"] == "middleClickEvent":
+            if macroEvents["events"][i]["pressed"] == True:
+                mouseControl.press(Button.middle)
+            else:
+                mouseControl.release(Button.middle)
+        elif macroEvents["events"][i]["type"] == "scrollEvent":
+            mouseControl.scroll(macroEvents["events"][i]["dx"], macroEvents["events"][i]["dy"])
+        elif macroEvents["events"][i]["type"] == "keyboardEvent":
+            keyToPress = macroEvents["events"][i]["key"] if 'Key.' not in macroEvents["events"][i]["key"] else special_keys[macroEvents["events"][i]["key"]]
+            if macroEvents["events"][i]["pressed"] == True:
+                keyboardControl.press(keyToPress)
+            else:
+                keyboardControl.release(keyToPress)
--- software.py
+++ software.py
@@ -1,19 +1,55 @@
+import threading
 from tkinter import *
+from tkinter.ttk import *
 
+from macro import startRecord, stopRecord, playRec
+
+# Window Setup
 window = Tk()
 window.title("MacroRecorder")
-window.geometry("500x200")
+window.geometry("350x200")
 window.iconbitmap("assets/logo.ico")
 
 my_menu = Menu(window)
 window.config(menu=my_menu)
 
-def cmd():
-    print('command')
 
-file_menu = Menu(my_menu,tearoff=0)
+def startRecordingAndChangeImg():
+    global stopBtn
+    recordBtn.pack_forget()
+    stopBtn = Button(window, image=stopImg, command=stopRecordingAndChangeImg)
+    stopBtn.pack(side=RIGHT, padx=50)
+    startRecord()
+    window.wait_variable()
+
+def stopRecordingAndChangeImg():
+    global recordBtn
+    stopBtn.pack_forget()
+    recordBtn = Button(window, image=recordImg, command=startRecordingAndChangeImg)
+    recordBtn.pack(side=RIGHT, padx=50)
+    stopRecord()
+
+
+# Menu Bar
+file_menu = Menu(my_menu, tearoff=0)
 my_menu.add_cascade(label="File", menu=file_menu)
-file_menu.add_command(label="New...", command=cmd)
-file_menu.add_command(label="Exit", command=window.quit)
+file_menu.add_command(label="New")
+file_menu.add_command(label="Save", command=window.quit, state=DISABLED)
+file_menu.add_command(label="Save as", command=window.quit, state=DISABLED)
+file_menu.add_separator()
+file_menu.add_command(label="Settings", command=window.quit)
+
+# Play Button
+playImg = PhotoImage(file=r"assets/button/play.png")
+playBtn = Button(window, image=playImg, command=playRec)
+playBtn.pack(side=LEFT, padx=50)
+
+# Record Button
+recordImg = PhotoImage(file=r"assets/button/record.png")
+recordBtn = Button(window, image=recordImg, command=startRecordingAndChangeImg)
+recordBtn.pack(side=RIGHT, padx=50)
+
+# Stop Button
+stopImg = PhotoImage(file=r"assets/button/stop.png")
 
 window.mainloop()
