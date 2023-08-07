4f55eac14115500ea48512c98c5c332e7ed9dcc6
LE LOUER Lucas <lucaslelouer@gmail.com>
2023-08-07 17:47:20 +0200

    Added load file button

--- software.py
+++ software.py
@@ -2,7 +2,6 @@ import threading
 from tkinter import *
 from tkinter.ttk import *
 from pynput import keyboard
-from keyboard import is_pressed
 import subprocess
 import atexit
 import time
@@ -47,41 +46,6 @@ def on_release(key):
             file_menu.entryconfig('New', state=NORMAL, command=newMacro)
             recordSet = True
 
-keyboardControl = keyboard.Controller()
-
-
-
-def on_release(key):
-    global record2, playback2, recordBtn, lengthOfRecord
-    try:
-        if key.char == 'è':
-            if record2 == False:
-                startRecordingAndChangeImg(False)
-        if key.char == 'à':
-            if (record2 == False and playback2 == False):
-                replay(False)
-    except AttributeError:
-        if key == keyboard.Key.esc:
-            if record2 == True:
-                stopRecordingAndChangeImg(False)
-
-
-# def key_released(e):
-#     global record2, playback2
-#
-#     if e.keysym == 'Escape':
-#         if record2 == True:
-#             stopRecordingAndChangeImg(False)
-#
-#     if e.char == 'è':
-#         if (record2 == False and playback2 == False):
-#             startRecordingAndChangeImg(False)
-#     #
-#     if e.char == 'à':
-#         if (record2 == False and playback2 == False):
-#             replay(False)
->>>>>>> 7043313f60759a798761a735a23a23dd99d0135b
-
 
 def cleanup():
     if 'macro_process' in globals():
@@ -103,7 +67,6 @@ window.config(menu=my_menu)
 
 
 def startRecordingAndChangeImg(pressKey=True):
-<<<<<<< HEAD
     global stopBtn, record2, playback2
     playBtn.configure(state=DISABLED)
     if playback2 == False:
@@ -140,44 +103,6 @@ def replay(pressKey=True):
             keyboardControl.release('p')
         recordBtn.configure(state=DISABLED)
 
-=======
-    global stopBtn, lengthOfRecord, record2, playback2
-    if playback2 == False:
-        if pressKey:
-            keyboardControl.press('è')
-            keyboardControl.release('è')
-        lengthOfRecord = time.time()
-        recordBtn.configure(image=stopImg, command=stopRecordingAndChangeImg)
-        record2 = True
-        print('function start callled')
-
-
-def stopRecordingAndChangeImg(pressKey=True):
-    global recordBtn, lengthOfRecord, record2
-    if pressKey:
-        keyboardControl.press(keyboard.Key.esc)
-        keyboardControl.release(keyboard.Key.esc)
-    print(time.time() - lengthOfRecord)
-    lengthOfRecord = (time.time() - lengthOfRecord) + 2
-    recordBtn.configure(image=recordImg, command=startRecordingAndChangeImg)
-    playBtn.configure(state=NORMAL)
-    print('record stoppé')
-    record2 = False
-
-def replay(pressKey=True):
-    global playback2, recordBtn
-    playback2 = True
-    if pressKey:
-        keyboardControl.press('à')
-        keyboardControl.release('à')
-    recordBtn.configure(state=DISABLED)
-    threading.Thread(target=buttonDisabledToEnable).start()
-
-def buttonDisabledToEnable():
-    global playback2, lengthOfRecord
-    time.sleep(lengthOfRecord)
-    recordBtn.configure(state=NORMAL)
-    playback2 = False
 
 
 def saveMacro():
@@ -231,7 +156,6 @@ recordBtn.pack(side=RIGHT, padx=50)
 # Stop Button
 stopImg = PhotoImage(file=r"assets/button/stop.png")
 
-# window.bind('<KeyRelease>',key_released )
 
 keyboardListener = keyboard.Listener(on_release=on_release)
 keyboardListener.start()
