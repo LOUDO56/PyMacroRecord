8e733ea76000c15b7e0de7a4b8953b49ba5af0e3
LE LOUER Lucas <lucaslelouer@gmail.com>
2023-08-23 16:29:41 +0200

    Added download page

---
+++ script.js
@@ -0,0 +1,11 @@
+const hamburger_icon = document.querySelector('.hambuger-icon');
+const close_menu_icon = document.querySelector('.close-icon');
+const mobile_menu = document.querySelector('.links-mobile-menu')
+
+hamburger_icon.addEventListener('click', () => {
+    mobile_menu.style.right = "0";
+})
+
+close_menu_icon.addEventListener('click', () => {
+    mobile_menu.style.right = "-300px";
+})
