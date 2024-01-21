c3a0560054bd24154fe33faac4b027964ec73172
LE LOUER Lucas <lucaslelouer@gmail.com>
2024-01-21 15:23:28 +0100

    Added missing F8,F9... key

--- src/macro.py
+++ src/macro.py
@@ -25,9 +25,11 @@ special_keys = {
     "Key.alt_l": Key.alt_l, "Key.alt_r": Key.alt_r, "Key.cmd": Key.cmd, "Key.cmd_l": Key.cmd_l,
     "Key.cmd_r": Key.cmd_r, "Key.enter": Key.enter, "Key.backspace": Key.backspace, "Key.f19": Key.f19,
     "Key.f18": Key.f18, "Key.f17": Key.f17, "Key.f16": Key.f16, "Key.f15": Key.f15, "Key.f14": Key.f14,
-    "Key.f13": Key.f13, "Key.media_volume_up": Key.media_volume_up, "Key.media_volume_down": Key.media_volume_down,
+    "Key.f13": Key.f13, "Key.f12": Key.f12, "Key.f11": Key.f11, "Key.f10": Key.f10,
+    "Key.f9": Key.f9, "Key.f8": Key.f8, "Key.f7": Key.f7, "Key.f6": Key.f6, "Key.f5": Key.f5,
+    "Key.f4": Key.f4, "Key.f3": Key.f3, "Key.f2": Key.f2, "Key.f1": Key.f1,
+    "Key.media_volume_up": Key.media_volume_up, "Key.media_volume_down": Key.media_volume_down,
     "Key.media_volume_mute": Key.media_volume_mute, "Key.media_play_pause": Key.media_play_pause,
-    "Key.f6": Key.f6, "Key.f5": Key.f5, "Key.f4": Key.f4, "Key.f3": Key.f3, "Key.f2": Key.f2, "Key.f1": Key.f1,
     "Key.insert": Key.insert, "Key.right": Key.right, "Key.down": Key.down, "Key.left": Key.left,
     "Key.up": Key.up, "Key.page_up": Key.page_up, "Key.page_down": Key.page_down, "Key.home": Key.home,
     "Key.end": Key.end, "Key.delete": Key.delete, "Key.space": Key.space,
@@ -179,6 +181,7 @@ def playRec():
                 if macroEvents["events"][events]["key"] != None:
                     keyToPress = macroEvents["events"][events]["key"] if 'Key.' not in macroEvents["events"][events]["key"] else \
                     special_keys[macroEvents["events"][events]["key"]]
+                    print(keyToPress)
                     if playback == True:
                         if macroEvents["events"][events]["pressed"] == True:
                             keyboardControl.press(keyToPress)
