f78c1817c6994dc495d59852f68085e0cfb489df
LOUDO <lucaslelouer@gmail.com>
2024-02-19 10:40:22 +0100

    Fixed no internet crash

--- src/utils/version.py
+++ src/utils/version.py
@@ -12,11 +12,14 @@ class Version:
     def checkVersion(self):
         api_url = f'https://api.github.com/repos/LOUDO56/PyMacroRecord/releases/latest'
 
-        response = requests.get(api_url)
+        try:
+            response = requests.get(api_url)
 
-        if response.status_code == 200:
-            release_data = response.json()
-            self.new_version = release_data['tag_name'].replace('v', '')
-            return "Outdated" if self.new_version != self.version else "Up to Date"
-        else:
+            if response.status_code == 200:
+                release_data = response.json()
+                self.new_version = release_data['tag_name'].replace('v', '')
+                return "Outdated" if self.new_version != self.version else "Up to Date"
+            else:
+                return "Cannot fetch if new update"
+        except:
             return "Cannot fetch if new update"
