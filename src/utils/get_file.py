0104774b37fa5cb98bf2ee2efbb2aba6693d0d34
LOUDO <lucaslelouer@gmail.com>
2026-04-05 22:09:21 +0200

    fix: resource path pyinstaller one file

--- src/utils/get_file.py
+++ src/utils/get_file.py
@@ -5,7 +5,7 @@ from os import path
 def resource_path(relative_path):
     """Get absolute path to resource, works for dev, PyInstaller and cx_Freeze."""
     if getattr(sys, "frozen", False):
-        base_path = path.dirname(sys.executable)
+        base_path = getattr(sys, "_MEIPASS", path.dirname(sys.executable))
     else:
         base_path = path.abspath(".")
     return path.join(base_path, relative_path)
