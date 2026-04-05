b757ecc5696d126dd7353cd5aefa2517422e1761
LOUDO <lucaslelouer@gmail.com>
2026-04-05 21:01:42 +0200

    fix: realname -> realpath

--- src/main.py
+++ src/main.py
@@ -10,5 +10,5 @@ if platform.lower() == "win32":
     ctypes.windll.shcore.SetProcessDpiAwareness(PROCESS_PER_MONITOR_DPI_AWARE)
 
 if __name__ == "__main__":
-    os.chdir(os.path.dirname(os.path.realname(__file__)))
+    os.chdir(os.path.dirname(os.path.realpath(__file__)))
     MainApp()
